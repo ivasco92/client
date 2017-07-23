3da79712321185e4d85d81eb5f7c762d2b4f52f3
p.pepe <p.pepe@00e02c96-b8b1-4a9b-8fae-107466490ca7>
2017-07-23 18:06:39 +0000

    #7864 fix bug e fix gui

--- trunk/Codice/client/app/src/main/java/com/app/smartmuseum/smartmuseum/InfoActivity.java
+++ trunk/Codice/client/app/src/main/java/com/app/smartmuseum/smartmuseum/InfoActivity.java
@@ -1,8 +1,14 @@
 package com.app.smartmuseum.smartmuseum;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.view.WindowManager;
+
+import static com.app.smartmuseum.smartmuseum.R.id.toolbar;
+
 
 public class InfoActivity extends AppCompatActivity {
 
@@ -14,6 +20,21 @@ public class InfoActivity extends AppCompatActivity {
         //Fullscreen schermo
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar3);
+        setSupportActionBar(toolbar);
+
+        //back arrow
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
 
     }
 }
